603007c6507752be05a1815f242d5e7601e31f66
drlaplace <43067096+drlaplace@users.noreply.github.com>
2025-09-04 07:45:24 +0200

    Delete KVV_Departure_Monitor/custom_components/KVV_Departure_Monitor directory

--- KVV_Departure_Monitor/custom_components/KVV_Departure_Monitor/const.py
+++
@@ -1,8 +0,0 @@
-"""Constants for the KVV_Departure_Monitor integration."""
-
-DOMAIN = "KVV_Departure_Monitor"
-
-DEFAULT_SCAN_INTERVAL = 60
-MIN_SCAN_INTERVAL = 10
-CONF_STOP = "stop"
-CONF_SCAN_INTERVAL = 60
